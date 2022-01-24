b5960a5bbaea82ed046c803d2d84085371337baf
NikitaBuklanov <buklanovnik@gmail.com>
2022-01-24 23:01:25 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #1. GIT\n";
     cout << "Variant #3. Precipitation\n";
     cout << "Author: Nikita Buklanov\n";
+    cout << "Group: 14\n";
     return 0;
 }
